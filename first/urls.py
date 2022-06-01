8db0040e0596ce358a17c6a02090a76ebae5455b
RISHEL PK <rishelpk007@gmail.com>
2022-06-01 10:12:22 +0530

    first commit

---
+++ first/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from.import views
+
+urlpatterns = [
+    path ('m1',views.m2, name="rish"),
+    path ('m4',views.m3 ,name='son')
+    
+    
+]
---
+++ first/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+def m2 (request):
+    return render(request,'fb/facebk.html')
+def m3 (request):
+    return render(request,'fb/scnd.html')
