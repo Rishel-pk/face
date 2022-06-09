14f248f3cf82b7d9e71935ab3f6018e8ad42858a
RISHEL PK <rishelpk007@gmail.com>
2022-06-09 12:32:43 +0530

    sm

--- first/urls.py
+++ first/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path ('m1',views.m2, name="rish"),
+    path ('',views.m2, name="rish"),
     path ('m4',views.m3 ,name='son')
     
     
